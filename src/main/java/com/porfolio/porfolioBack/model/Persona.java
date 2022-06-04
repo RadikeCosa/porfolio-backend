98b75c08f0ddc6d25d258e989767893ad74eb54b
RadikeCosa <ramirocosa@gmail.com>
2022-06-04 11:10:54 -0300

    primer commit

---
+++ src/main/java/com/porfolio/porfolioBack/model/Persona.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.porfolio.porfolioBack.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Ramiro
+ */
+@Getter @Setter
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String linkedin;
+    private String github;
+    private String twitter;
+    private String instagram;
+    
+    
+}
---
+++ src/main/java/com/porfolio/porfolioBack/repository/IPersonaRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.porfolio.porfolioBack.repository;
+
+import com.porfolio.porfolioBack.model.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Ramiro
+ */
+public interface IPersonaRepository extends JpaRepository <Persona, Long>
+{
+    
+}
